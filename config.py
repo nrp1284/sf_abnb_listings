43cb26ba8506bb1363d166fb2bb2149cf84e131c
jmarrujo31 <89328181+jmarrujo31@users.noreply.github.com>
2022-01-08 09:42:53 -0800

    SQL testing

---
+++ config.py
@@ -0,0 +1,35 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "63f283b9",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "db_password = 'sjSharks31'"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "mlenv",
+   "language": "python",
+   "name": "mlenv"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.11"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
