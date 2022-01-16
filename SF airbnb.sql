82feac2265211c1c8b5961d51d8c85f12160602d
jmarrujo31 <maj_31@yahoo.com>
2022-01-15 19:51:06 -0800

    update SQL join

--- SF airbnb.sql
+++ SF airbnb.sql
@@ -1,45 +1,20 @@
---create table 1
-CREATE TABLE abnb (
-    ID INT NOT NULL,
-     host_response_time VARCHAR NOT NULL,
-     host_response_rate INT NOT NULL,
-     host_listings_count INT NOT NULL,
-     last_name VARCHAR NOT NULL,
-     neighbourhood VARCHAR NOT NULL,
-     neighbourhood_cleansed VARCHAR NOT NULL,
- PRIMARY KEY (id));
- 
- --create table 2
- CREATE TABLE abnbacc (
-     ID INT NOT NULL,
-     property_type VARCHAR NOT NULL,
-     room_type VARCHAR NOT NULL,
-     accommodates INT NOT NULL,
-     bathrooms INT NOT NULL,
-     bedrooms INT NOT NULL,
-     beds INT NOT NULL,
-	 bed_type VARCHAR NOT NULL,
- PRIMARY KEY (id));
 
+-- still need to add all columns
  --left join 
-SELECT abnb.ID,
- 	abnb.neighbourhood_cleansed,
-    abnbacc.property_type,
-    abnbacc.room_type,
-    abnbacc.accommodates
-FROM abnb
-LEFT JOIN abnbacc
-ON abnb.ID = abnbacc.ID;
+SELECT listings.id,
+	listings.zipcode,
+ 	listings.neighbourhood_cleansed,
+	sfmasszip.land_area_sqmeters	
+FROM listings
+LEFT JOIN sfmasszip
+ON listings.zipcode = sfmasszip.zip_code;
 
---left join into new table 'abnb_clean' 
-SELECT 
-	abnb.ID,
- 	abnb.neighbourhood_cleansed,
-    abnbacc.property_type,
-    abnbacc.room_type,
-    abnbacc.accommodates
-INTO abnb_clean
-FROM abnb
-LEFT JOIN abnbacc
-ON abnb.ID = abnbacc.ID
-;
+
+
+-- alter column type
+ALTER TABLE listings
+ALTER COLUMN zipcode TYPE VARCHAR; 
+
+ALTER TABLE sfmasszip
+ALTER COLUMN zip_code TYPE VARCHAR; 
+
