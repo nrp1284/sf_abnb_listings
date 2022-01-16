165742a37e80782289786f033b4f690181d3c4b2
jmarrujo31 <maj_31@yahoo.com>
2022-01-15 22:14:42 -0800

    sql

--- SF airbnb.sql
+++ SF airbnb.sql
@@ -1,20 +1,40 @@
-
--- still need to add all columns
- --left join 
-SELECT listings.id,
-	listings.zipcode,
- 	listings.neighbourhood_cleansed,
-	sfmasszip.land_area_sqmeters	
-FROM listings
-LEFT JOIN sfmasszip
-ON listings.zipcode = sfmasszip.zip_code;
-
-
-
 -- alter column type
 ALTER TABLE listings
 ALTER COLUMN zipcode TYPE VARCHAR; 
 
 ALTER TABLE sfmasszip
-ALTER COLUMN zip_code TYPE VARCHAR; 
+ALTER COLUMN land_area_sqmeters TYPE VARCHAR;
 
+SELECT * FROM listings
+LEFT JOIN sfmasszip
+ON listings.zipcode = sfmasszip.zip_code;
+
+ 
+
+
+--change column type date varchar
+ALTER TABLE listings 
+ALTER COLUMN first_review SET DATA TYPE date
+      USING to_date(first_review, 'mm-dd-yyyy');
+
+ALTER TABLE listings
+ALTER COLUMN host_is_superhost TYPE VARCHAR; 
+
+
+
+SELECT listings.id,
+	listings.host_since,
+	listings.host_response_time,
+	listings.host_response_rate,
+	listings.host_is_superhost,
+	listings.host_listings_count,
+	listings.host_total_listings_count,
+	listings.host_identity_verified,
+ 	listings.neighbourhood_cleansed,
+	listings.zipcode,
+	listings.latitude,
+	listings.longitude,
+	sfmasszip.land_area_sqmeters	
+FROM listings
+LEFT JOIN sfmasszip
+ON listings.zipcode = sfmasszip.zip_code;
